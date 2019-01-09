6505d7010bd3cf6df5ac1603125426b93b9927e7
huangchen1996 <huangchen19960227@163.com>
2019-01-09 18:47:15 +0800

    更多的变量和打印

---
+++ Python_Automation/Chapter_01_Basic/Practice_05_FormatString.py
@@ -0,0 +1,30 @@
+#字符串是程序将信息展示给人的方式
+
+
+#我的名字是huangchen
+my_name = 'huangchen'
+
+#我的年龄是22岁
+my_age = 22;
+
+#我的体重是110斤
+my_height = 110
+
+#我的身高是175
+my_weight = 175
+
+#我的眼睛是黑色的
+my_eyes = 'black'
+
+#我的牙齿是白色的
+my_teeth = 'White'
+
+#我的头发是棕色的
+my_hair = 'Brown'
+
+print("让我们来讨论 %s." % my_name)
+print("他是 %d 高." % my_height)
+print("他是 %d 净重.")
+print("他有一双%s眼睛和一头%s的头发." % (my_eyes, my_hair))
+print("他的牙齿在咖啡里通常是%s." % my_teeth)
+print("If I add %d,%d,and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
