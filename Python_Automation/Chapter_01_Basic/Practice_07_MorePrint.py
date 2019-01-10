8cdde8bb9986171dd3a859245e9ff223a77bb818
huangchen1996 <huangchen19960227@163.com>
2019-01-10 14:12:31 +0800

    更多打印

---
+++ Python_Automation/Chapter_01_Basic/Practice_07_MorePrint.py
@@ -0,0 +1,23 @@
+print("玛丽有一个小羊羔。")
+print("它的羊毛白的像 %s." % 'snow')
+print("mali将要去哪儿.")
+print("."*10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "e"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#将end1,2,3,4,5,6字符连接在一起
+print(end1 + end2 + end3 + end4 + end5 + end6, )
+
+#将end7,8,9,10,11,12字符连接在一起
+print(end7 + end8 + end9 + end10 + end11 + end12)
