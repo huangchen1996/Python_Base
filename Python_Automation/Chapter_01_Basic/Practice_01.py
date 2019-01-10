3a0f864ccedd369a3533177764abf302348cb61d
huangchen1996 <huangchen19960227@163.com>
2019-01-10 09:39:43 +0800

    字符串和文本（附加强化题）

--- Python_Automation/Chapter_01_Basic/Practice_01.py
+++ Python_Automation/Chapter_01_Basic/Practice_01.py
@@ -1,8 +1,27 @@
 #输出信息到控制台
-print ("hello world!")
-print ("Hello Again!")
-print ("I like typing this")
-print ("This is fun")
-print ("Yay! Printing.")
-print ("I d much rather you 'not'")
-print ('I "said" do not touch this')
+#这是第三题的答案
+print("hello world!")
+print("Hello Again!")
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing.")
+print("I d much rather you 'not'")
+print('I "said" do not touch this')
+
+#习题
+#1让脚本多打印一行(7 + 1行)
+print("hello world!")
+print("Hello Again!")
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing.")
+print("I d much rather you 'not'")
+print('I "said" do not touch this')
+print("这就是多打印的那一行")
+
+#2让脚本只打印一行
+print("仅仅只打印了一行")
+
+#3在一行的起始位置加一个#号，并研究他的作用
+#第三题的答案在上面
+
