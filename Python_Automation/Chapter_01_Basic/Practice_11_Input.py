6e4242e567724687f0d75df546584103ce459e27
huangchen1996 <huangchen19960227@163.com>
2019-01-11 14:03:15 +0800

    提问

---
+++ Python_Automation/Chapter_01_Basic/Practice_11_Input.py
@@ -0,0 +1,21 @@
+'''
+一般软件做的事主要是以下三件
+1，接收人的输入
+2，改变输入
+3，打印出改变了的输入
+'''
+
+print("你现在多大了?",)
+
+age = input()
+
+print("你的身高?", )
+
+height = input()
+
+print("你的体重是多少?",)
+
+weight = input()
+
+print("所以,你有 %s 岁,%s 高 并且体重 %s斤 ." % (age, height, weight))
+
