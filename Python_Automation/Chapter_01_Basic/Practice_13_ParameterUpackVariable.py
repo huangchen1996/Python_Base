e1094a18f9a5a05cfb46d94a99ec80b32e93e6f4
huangchen1996 <huangchen19960227@163.com>
2019-01-11 14:45:36 +0800

    参数解包变量

---
+++ Python_Automation/Chapter_01_Basic/Practice_13_ParameterUpackVariable.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+
+script, first, second, third = argv
+
+print("这个script就叫做:", script)
+print("你的第一个变量是:", first)
+print("你的第二个变量是:", second)
+print("你的第三个变量是:", third)
+
+print("\n")
+
