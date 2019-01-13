9505ca0f8a20cc1add45b1c8bfa0d5f79b480913
huangchen1996 <huangchen19960227@163.com>
2019-01-14 00:37:06 +0800

    IfAndOr

---
+++ PythonBase/IfAndOr/If.py
@@ -0,0 +1,5 @@
+PersonComputer = input("MacbookPro可以么?")
+NoteComputer = input("华硕玩家国度喜欢么?")
+if PersonComputer == "可以" or NoteComputer == "喜欢":
+    print("那你放心买吧，都挺不错的")
+
---
+++ PythonBase/IfAndOr/if_02.py
@@ -0,0 +1,10 @@
+color = input("你的肤色是什么颜色?")
+money = input("你每天花多少钱?")
+beautiful = input("你觉得自己漂亮么?")
+if color == "白" and money ==1000 and beautiful == "漂亮":
+    print("你是一个标准白富美")
+else:
+    print("革命尚未成功，同志仍需加油")
+
+
+
