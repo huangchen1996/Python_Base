d78bacebc8d5c923be76a371595f9f6e90f46e8b
huangchen1996 <huangchen19960227@163.com>
2019-01-17 09:54:38 +0800

    猜拳游戏

---
+++ PythonBase/IfAndOr/If_Application_04.py
@@ -0,0 +1,13 @@
+import random
+
+player = input('请输入:剪刀(0) 石头(1) 布(2):')
+
+player = input(player)
+
+computer = random.randint(0, 2)
+computer = input(computer)
+
+if (player == 0) and (computer == 2) or (player == 1) and (player == 0) or (player ==2) and (computer == 1):
+    print("你赢了")
+else:
+    print("你输了")
