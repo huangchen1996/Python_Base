d867c6ddae8cae367b9f096499d5200369a91652
huangchen1996 <huangchen19960227@163.com>
2019-01-21 11:18:27 +0800

    类和对象

---
+++ OOP/ClassAndObject_01/Annimal_02.py
@@ -0,0 +1,10 @@
+class Annimal_02:
+	def shout(self):
+		print("遇见陌生人汪汪叫")
+
+	def shake(self):
+		print("遇到熟人摇尾巴")
+
+haha    =   Annimal_02()
+haha.shout()
+haha.shake()
---
+++ OOP/ClassAndObject_01/Person_01.py
@@ -0,0 +1,15 @@
+class Person_01:
+
+	def run(self):
+		print("xiaoming每天早上跑完步就会去吃早饭")
+
+#创建小明的对象
+xiaoming    =   Person_01()
+
+xiaoming.run()
+
+
+
+
+
+
